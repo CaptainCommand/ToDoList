5b4b6b27e7125533f07854f4f09f6a539b1628cd
George Wulfers <george.wulfers@gmail.com>
2016-01-29 20:38:11 -0800

    Starting Point UE4.8
    This is the starting point of the Repo 's life.

---
+++ ToDoList/Intermediate/Build/Win64/UE4Editor/Development/ToDoList/PCH.ToDoList.h.cpp
@@ -0,0 +1,1 @@
+#include "C:\Users\George\Documents\GitHub\GGJ\ToDoList\ToDoList\Source\ToDoList\ToDoList.h"
---
+++ ToDoList/Source/ToDoList/ToDoListGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "ToDoListGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TODOLIST_API AToDoListGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
